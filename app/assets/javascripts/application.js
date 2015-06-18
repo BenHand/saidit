834a01e04c9dc85e124fe999b149b7a67d9ce335
Benjamin Hand <gneeshot@gmail.com>
2015-06-17 19:08:57 -0500

    set an eventlogger to keep track of how many times header is clicked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,15 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$(document).ready(function() {
+    var firstLink = $('.link-show').first();
+    $('.board-new').hover(
+        function() { firstLink.addClass('highlight'); },
+        function() { firstLink.removeClass('highlight'); }
+    );
+    var headerCounter = 0;
+    $('header.row').on('click', function() {
+        console.log('Header has been clicked ' + headerCounter++ + ' times.');
+    });
+});
+
